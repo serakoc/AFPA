fc79523e6a1a027e23428b5f2d58c2cd854a189a
kraken <cpserakoc@outlook.fr>
2020-01-14 16:35:29 +0100

    final

--- JS/Exercice5.js
+++ JS/Exercice5.js
@@ -1,11 +1,11 @@
-//on crée une fonction qui executeras une instruction 10 fois : 1 fois pour chaque multiplication
+//on crï¿½e une fonction qui executeras une instruction 10 fois : 1 fois pour chaque multiplication
 //la mutlitplication est effectuer avec la variable i qui est incrementer a chaque tour de boucle
 //ce qui simule la ultiplication de 1 a 10 sur la variable chiffre
         function Table(chiffre)
         {
             for(var i=1; i<=10; i++)
             {  
-                console.log(chiffre+" * "+i+" = "+(chiffre * i));
+                alert(chiffre+" * "+i+" = "+(chiffre * i));
             }
         }
         Table(10);
